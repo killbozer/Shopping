a2990927ec6bb8b51d6febbefe6c26ecc97fbb34
Matthew James Wheelhouse <S301012@students.cdu.edu.au>
2018-08-31 16:13:50 +0930

    able to add items gear

--- scripts.js
+++ scripts.js
@@ -1,52 +1,72 @@
-function saveData() {
-	var btnInput = document.getElementById('save');
-	var itmInput = document.getElementById('item');
-	var itmId = 0
+document.addEventListener('DOMContentLoaded', function() {
 	
-	btnInput.addEventListener('click', function(evt) {
-		evt.preventDefault();
+	var db = undefined;
+
+	function saveData() {
+		var btnInput = document.getElementById('save');
+		var itmInput = document.getElementById('item');
 		
-		var transaction = db.transaction('items', 'readwrite');
-		var store = transaction.objectStore('items');
-		
-		var data = store.add({id: itmId, name: itmInput.value});
-		data.addEventListener('success', function(evt) {
-			console.log('success', evt.target.result);
-		}	
+		btnInput.addEventListener('click', function(evt) {
+			evt.preventDefault();
+			console.log('clicked');
+			
+			var transaction = db.transaction('items', 'readwrite');
+			console.log('transaction', transaction);
+			var store = transaction.objectStore('items');
+			
+			var data = store.add({name: itmInput.value});
+			data.addEventListener('success', function(evt) {
+				console.log('success', evt.target.result);
+			});
+			data.addEventListener('error', function(err) {
+				console.log(err);
+			});
+		});
 	}
-}
 
 
 
 
-function connectToDB() {
-  // Connect to database
-  var conn = indexedDB.open('test-db', 5);
-  console.log('conn');
+	function connectToDB() {
+	  // Connect to database
+	  var conn = indexedDB.open('test-db', 5);
+	  console.log('conn');
 
-  // Listen for success and bind save button and load data
-  conn.addEventListener('success', function(evt) {
-    console.log('connected event', evt)
-    var db = evt.target.result;
-    console.log(db);
+	  // Listen for success and bind save button and load data
+	  conn.addEventListener('success', function(evt) {
+		console.log('connected event', evt)
+		db = evt.target.result;
+		console.log(db);
 
-  });
+	  });
 
-  // Listen for error event and show the error
-  conn.addEventListener('error', function(evt) {
-    console.log('error connecting', evt.target.error);
-  });
+	  // Listen for error event and show the error
+	  conn.addEventListener('error', function(evt) {
+		console.log('error connecting', evt.target.error);
+	  });
 
-  // Listen for the uppgradeneeded event
-  // Create an object store to track people
-  // use keyPath to store the ID value
-  conn.addEventListener('upgradeneeded',function(evt) {
-    console.log('upgrade needed', evt)
-     var db = evt.target.result;
-    console.log(db);
+	  // Listen for the uppgradeneeded event
+	  // Create an object store to track people
+	  // use keyPath to store the ID value
+	  conn.addEventListener('upgradeneeded',function(evt) {
+		console.log('upgrade needed', evt)
+		var db = evt.target.result;
+		console.log(db);
+		
+		var objectStore = db.createObjectStore("items", { keyPath: "id", autoIncrement: true });
+		console.log(objectStore);
+		objectStore.createIndex('name', 'item', {unique: false});
+		objectStore.transaction.addEventListener('complete', function(evt) {
+		 console.log('Store created');
+		});
 
-  });
-}
+	  });
+	}
 
-console.log('reload');
-document.addEventListener('DOMContentLoaded', connectToDB);
+	console.log('reload');
+	
+	document.getElementById('save').addEventListener('click', saveData);
+	
+	connectToDB();
+	
+});
